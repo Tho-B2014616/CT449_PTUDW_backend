7e56c67988e1af35170176e89904dbb501f3273b
Tho-b2014616 <thob2014616@student.ctu.edu.vn>
2024-03-09 21:10:59 +0700

    Cai dat cac handler truy xuat CSDL

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -3,17 +3,18 @@ const contact = require("../controllers/contact.controller");
 
 const router = express.Router();
 
-router.route("/")
-    .get(contact.findAll)
-    .post(contact.create)
-    .delete(contact.deleteAll);
+router
+  .route("/")
+  .get(contact.findAll)
+  .post(contact.create)
+  .delete(contact.deleteAll);
 
-router.route("/favorite")
-    .get(contact.findAllFavorites);
+router.route("/favorite").get(contact.findAllFavorite);
 
-router.route("/:id")
-    .get(contact.findOne)
-    .put(contact.update)
-    .delete(contact.delete);
+router
+  .route("/:id")
+  .get(contact.findOne)
+  .put(contact.update)
+  .delete(contact.delete);
 
 module.exports = router;
